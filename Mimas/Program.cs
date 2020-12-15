fdaff6af7823ff51c27b143175e38fd4a5d7173d
ИСТОРИН ВЛАД <xloksik3@gmail.com>
2020-12-15 13:16:12 +0200

    Добавьте файлы проекта.

---
+++ Mimas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string z = "Мимас — спутник Сатурна, открытый 17 сентября 1789 года Уильямом Гершелем. Назван в честь Мимаса — одного из титанов греческой мифологии.";
+            Console.WriteLine(z.Trim().Remove(0,1));
+            Console.ReadKey();
+        }
+    }
+}
